93e4586f9890719b2c3ce933218651dee9496188
hamza eraoui <hamza.eraoui@oracle.com>
2023-11-08 17:32:46 +0100

    enhance the project creation process

--- bin/commands/create-project.js
+++ bin/commands/create-project.js
@@ -15,27 +15,25 @@ function initialize(projectName){
     const gitCheckoutCommand = `git clone --depth 1 git@github.com:Eraoui909/apex-initializr.git ${repoName}`; 
     const gitRemoveRemote = `git remote rm origin`; 
     
-    console.log(`Cloning the repo with name ${repoName}`);
+    console.log(`[1] - Cloning the repo with name ${repoName}`);
     const checkOut = runCommand(gitCheckoutCommand);
     if( !checkOut ) process.exit(-1);
 
     process.chdir(`${repoName}`);
 
-    console.log(`Removing remote origin from ${repoName}`);
+    console.log(`[2] - Removing remote origin from ${repoName}`);
     const rmRemote = runCommand(gitRemoveRemote);
     if( !rmRemote ) process.exit(-1);
     
-    console.log(`Installing dependencies for ${repoName}`);
+    console.log(`[3] - Installing dependencies for ${repoName}`);
     const installDeps = runCommand(`npm install`);
     if( !installDeps ) process.exit(-1);
 
-    runCommand(`pwd`);
-
-    console.log(`Init configuration for ${repoName}`);
+    console.log(`[1] - Init configuration for ${repoName}`);
     const initConfig = runCommand(`./config/scripts/init-project.sh`);
     if( !initConfig ) process.exit(-1);
     
-    console.log(`Congratulations! your project has been installed`);
+    console.log(`[Done] - Congratulations! your project has been installed`);
         
 }
 
